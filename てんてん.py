a280a19676cbec4032864c22718c8a1db1d8066d
fuurin0111 <159630653+fuurin0111@users.noreply.github.com>
2024-02-11 18:15:36 +0900

    v2

--- てんてん.py
+++ てんてん.py
@@ -8,15 +8,19 @@ image_url = image_name
 im_gray_url = image_name+'_gray.png'
 new_im_gray_url = image_name+'_gray.png'
 
-image = cv2.imread(image_url)
-print(image.shape)
-print(image.dtype)
-
 im_gray = cv2.imread(image_url, cv2.IMREAD_GRAYSCALE)
 print(im_gray.shape)
 im_gray_y,im_gray_x = im_gray.shape
 print(im_gray.dtype)
 cv2.imwrite(im_gray_url, im_gray)
+
+if (im_gray_y > 350 and im_gray_x < 700) or (im_gray_x > 600 and im_gray_x < 1200):
+    im_gray = cv2.resize(im_gray, dsize=None, fx=0.5, fy=0.5)
+    im_gray_y,im_gray_x = im_gray.shape
+elif (im_gray_y >= 700 and im_gray_x < 1400) or (im_gray_x < 2400 and im_gray_x >= 1200):
+    im_gray = cv2.resize(im_gray, dsize=None, fx=0.25, fy=0.25)
+    im_gray_y,im_gray_x = im_gray.shape
+    
 
 new_im_gray = []
 img_size = 2
@@ -24,7 +28,7 @@ a = []
 for i in range(round(im_gray_y/img_size)):
     a = []
     for j in range(round(im_gray_x/img_size)):
-        if round(im_gray[i*img_size][j*img_size]/64) == 4:
+        if round(im_gray[i*img_size][j*img_size]/128) == 2:
               a.append("　")
         else:
               a.append("⬜")
